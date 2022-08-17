96650231c00a8feabd474046c5b536c8ce289eff
Sanya420 <sanjarmaulkan2102@mail.ru>
2022-08-17 22:33:09 +0600

    lets goooo

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,12 @@ func (app *application) routes() http.Handler {
 	mux.Post("/sec/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/sec/:id", dynamicMiddleware.ThenFunc(app.showsnippet))
 
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
