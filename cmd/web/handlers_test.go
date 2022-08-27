8bc6d9d84cff779bcfb99a50af8b0ea72b69ea62
Sanya420 <sanjarmaulkan2102@mail.ru>
2022-08-27 21:56:45 +0600

    lets goooo

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/sec/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/sec/2", http.StatusNotFound, nil},
+		{"Negative ID", "/sec/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/sec/1.23", http.StatusNotFound, nil},
+		{"String ID", "/sec/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/sec/", http.StatusNotFound, nil},
+		{"Trailing slash", "/sec/1/", http.StatusNotFound, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+
+			}
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q", tt.wantBody)
+			}
+		})
+	}
+}
